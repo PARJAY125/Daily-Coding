301d9cd6beb398443b9de15eaf2321f7f18449f1
PARJAY125 <58838833+PARJAY125@users.noreply.github.com>
2022-06-28 14:33:20 +0800

    Commit #3 - Bibit Automation

---
+++ Bibit Automation/Bibit.py
@@ -0,0 +1,153 @@
+# targetedElement = document.getElementsByClassName("am-card")
+# for (let item of targetedElement) {
+#     console.log("https://app.bibit.id/reksadana/" + item.getAttribute("data-symbol") );
+# }
+
+import webbrowser
+
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1657")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD205")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD124")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1766")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD618")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1669")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD619")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD983")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD323")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD555")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1543")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD429")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD479")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1775")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD3448")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD3595")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD984")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD2046")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD322")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD3173")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD832")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD3454")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD66")
+
+# pasar uang
+#####
+
+import webbrowser
+
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD3561")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD556")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD13")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD994")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1544")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD214")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD3206")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD4046")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1436")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD3542")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1409")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD332")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD838")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1763")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD3487")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1721")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD841")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD860")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD578")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD83")
+
+# obligasi
+#####
+
+import webbrowser
+
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD216")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD985")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD998")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1653")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD337")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD334")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1628")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD409")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1538")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1541")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD423")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD412")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD424")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD3509")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1668")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD431")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1911")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD68")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD569")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1755")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1001")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1656")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1764")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1765")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD82")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD218")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD707")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1366")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD487")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD853")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1634")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD562")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD833")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD3672")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD2049")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1756")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD4221")
+
+# saham
+#######
+
+import webbrowser
+
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1669")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD424")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD4046")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1668")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD332")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD838")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1001")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1775")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD3487")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1721")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD3595")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD984")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD218")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1564")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1366")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD487")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD860")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD322")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD3173")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD832")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD3454")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD853")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1634")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1822")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD562")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD872")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD70")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1725")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1743")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD3531")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD3702")
+
+# syariah
+########
+
+import webbrowser
+
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD3191")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1655")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1564")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD523")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1551")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD1629")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD872")
+webbrowser.open_new_tab("https://app.bibit.id/reksadana/RD70")
+
+# other
+########
